f1532e3635784a4f348c958640dd010e808ae924
eunjeong.song <hellogine12@gmail.com>
2019-01-01 02:16:14 +0900

    MVC 를 위한 routes 정의

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import { userRouter } from './router';
+import { userRouter, videoRouter, globalRouter } from './routers';
+import routes from './routes';
 
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello from home');
-
-const handleProfile = (req, res) => res.send('You are my profile');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,8 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.get('/', handleHome);
-app.get('/profile', handleProfile);
-app.use('/user', userRouter);
+app.use('/', globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
