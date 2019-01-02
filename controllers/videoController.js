0f06b0b38d4515db6bf23d38912b7ebb313987f6
eunjeong.song <hellogine12@gmail.com>
2019-01-03 01:23:24 +0900

    Template Variables in Pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,9 @@
-export const home = (req, res) => res.render('home');
-export const search = (req, res) => res.send('Search');
-export const videos = (req, res) => res.send('videos');
-export const upload = (req, res) => res.send('upload');
-export const videoDetail = (req, res) => res.send('videoDetail');
-export const editVideo = (req, res) => res.send('editVideo');
-export const deleteVideo = (req, res) => res.send('deleteVideo');
+export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
+export const search = (req, res) => res.send('search', { pageTitle: 'Search' });
+export const upload = (req, res) => res.send('upload', { pageTitle: 'Upload' });
+export const videoDetail = (req, res) =>
+  res.send('videoDetail', { pageTitle: 'Video Detail' });
+export const editVideo = (req, res) =>
+  res.send('editVideo', { pageTitle: 'Edit Video' });
+export const deleteVideo = (req, res) =>
+  res.send('deleteVideo', { pageTitle: 'Delete Video' });
