4d826629f305695419d25b44a317b8da6d821e10
eunjeong.song <hellogine12@gmail.com>
2019-01-06 23:36:40 +0900

    add user 템플릿

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ export const login = (req, res) => res.render('login', { pageTitle: 'Login' });
 export const logout = (req, res) =>
   res.render('logout', { pageTitle: 'Logout' });
 export const userDetail = (req, res) =>
-  res.render('logout', { pageTitle: 'Logout' });
+  res.render('userDetail', { pageTitle: 'User Detail' });
 export const editProfile = (req, res) =>
   res.render('editProfile', { pageTitle: 'Edit Profile' });
 export const changePassword = (req, res) =>
