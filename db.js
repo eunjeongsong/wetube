6e6981528bff6887a4bbd3b6d642dc52d5cba539
eunjeong.song <hellogine12@gmail.com>
2019-01-06 23:58:53 +0900

    fake video 데이터 추가

---
+++ db.js
@@ -0,0 +1,62 @@
+export const videos = [
+  {
+    id: 11111,
+    title: 'Video awesome',
+    description: 'This is comethig I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 11111,
+      name: 'Enzyme',
+      email: 'Enzyme@email.com'
+    }
+  },
+  {
+    id: 22222,
+    title: 'Video awesome',
+    description: 'This is comethig I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 11112,
+      name: 'Enzyme',
+      email: 'Enzyme@email.com'
+    }
+  },
+  {
+    id: 33333,
+    title: 'Video awesome',
+    description: 'This is comethig I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 11113,
+      name: 'Enzyme',
+      email: 'Enzyme@email.com'
+    }
+  },
+  {
+    id: 44444,
+    title: 'Video awesome',
+    description: 'This is comethig I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 11114,
+      name: 'Enzyme',
+      email: 'Enzyme@email.com'
+    }
+  },
+  {
+    id: 55555,
+    title: 'Video awesome',
+    description: 'This is comethig I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 11115,
+      name: 'Enzyme',
+      email: 'Enzyme@email.com'
+    }
+  }
+];
