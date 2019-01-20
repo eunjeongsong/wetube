fdb4da21dc3c642e5a5f5fd42122e35219d3e678
eunjeong.song <hellogine12@gmail.com>
2019-01-21 00:06:20 +0900

    import models

--- init.js
+++ init.js
@@ -3,7 +3,10 @@ import app from './app';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.PORT | 4000;
+import './models/Video';
+import './models/Comment';
+
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${PORT}`);
